c45c308d605addc1cc4fd5f7463b0b391523c727
Thomas_D <t.dare@ludus-academie.com>
2023-06-13 12:22:19 +0200

    Map Generation Level 2

--- maps.js
+++ maps.js
@@ -8,19 +8,40 @@ function createTile(className) {
 }
 // ↓↓ Première Map ↓↓
 if (labyrinth.dataset.level == "1"){
-    for (let i = 0; i < 36; i++) {      
+    player.style.top = "4rem"
+    let tileCount = 0;
+    for (let i = 0; i < 36; i++) {    
+        tileCount++;  
         labyrinth.appendChild(createTile('wall'));
     }
     for (let i = 0; i < 9; i++) {
+        tileCount++;  
         if (i == 8){
             labyrinth.appendChild(createTile('exit'));
         }else{
             labyrinth.appendChild(createTile('path'));
         }
     }
-    for (let i = 0; i < 36; i++) {      
+    for (let i = 0; i < 36; i++) {   
+        tileCount++;     
         labyrinth.appendChild(createTile('wall'));
     }
 }
 
 // ↓↓ Seconde Map ↓↓
+if (labyrinth.dataset.level == "2"){
+    player.style.left = "4rem"
+    let tileCount = 0;
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            tileCount++;
+            if (tileCount == 77){
+                labyrinth.appendChild(createTile('exit'));
+            } else if (j == 4){
+                labyrinth.appendChild(createTile('path'));
+            } else {
+                labyrinth.appendChild(createTile('wall'));
+            }
+        }
+    }
+}
