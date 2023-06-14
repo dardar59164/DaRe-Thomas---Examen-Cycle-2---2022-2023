ca1388f05797bff0e5b4a9d0f71a4f305ef99d51
Thomas_D <t.dare@ludus-academie.com>
2023-06-14 11:43:03 +0200

    Jeu Fonctionnel

--- app.js
+++ app.js
@@ -1,83 +1,118 @@
 // ↓↓ CSS Related Objects↓↓
-const buttonElement = document.querySelector('button');
-const textAreaElement = document.querySelector('#CSS-Prompt');
+const buttonElement = document.querySelector("button");
+const textAreaElement = document.querySelector("#CSS-Prompt");
 
 // ↓↓ Level Selection Objets ↓↓
-const lvlActuel = document.querySelector('span.actuel');
-const lvlSelector = document.querySelector('#Indication-Niveaux');  
-const lvlTooltip = document.querySelector('#Tooltip-Niveaux');
-const leftArrow = document.querySelector('.arrow#Left');
-const rightArrow = document.querySelector('.arrow#Right');
+const lvlActuel = document.querySelector("span.actuel");
+const lvlSelector = document.querySelector(".prevent-select");
+const lvlTooltip = document.querySelector("#Tooltip-Niveaux");
+const leftArrow = document.querySelector(".arrow#Left");
+const rightArrow = document.querySelector(".arrow#Right");
+const niveaux = document.querySelectorAll(".niveau");
 
 // ↓↓ Game Objects ↓↓
-const labyrinth = document.querySelector('#Labyrinthe');
-const player = document.querySelector('#Player');
-const goal = document.querySelector('.exit');
+const labyrinth = document.querySelector("#Labyrinthe");
 
 // ↓↓ Level Selection ↓↓
-let currentLevel = parseInt(labyrinth.dataset.level)
+const totalLevel = 6;
+let currentLevel = 1;
 
 window.onload = () => {
-    lvlActuel.innerHTML = labyrinth.dataset.level;
+  lvlActuel.innerHTML = currentLevel;
+  leftArrow.classList.add("disabled");
+  generateMap(currentLevel);
 };
-
-leftArrow.addEventListener('click', ()=>{
-    if (currentLevel > 1){
-        currentLevel--;
-        labyrinth.dataset.level = currentLevel.toString()
-        lvlActuel.innerHTML = labyrinth.dataset.level;
-    }
-})
-rightArrow.addEventListener('click', ()=>{
-    if (currentLevel < 6){
-        currentLevel++;
-        labyrinth.dataset.level = currentLevel.toString()
-        lvlActuel.innerHTML = labyrinth.dataset.level;
-    }
-})
-// ↓↓ CSS Text Logic
-buttonElement.addEventListener('click', () =>{
-    let css = textAreaElement.value;
-    if (!css.includes("transform:")){
-        alert("Mauvaise Réponse");
-    }else{
-        player.style.cssText = css;
-    }
+lvlSelector.addEventListener("click", (event) => {
+  lvlTooltip.classList.toggle("hidden");
+  lvlTooltip.classList.contains("hidden")
+    ? (lvlTooltip.style.display = "none")
+    : (lvlTooltip.style.display = "grid");
+  event.stopPropagation();
 });
-textAreaElement.addEventListener("keydown", (event) =>{
-    if (event.key == 'Enter'){
-        event.preventDefault();
-        buttonElement.click();
-    }
-})
+leftArrow.addEventListener("click", () => {
+  if (currentLevel > 1) {
+    currentLevel--;
+    currentLevel == 1
+      ? leftArrow.classList.add("disabled")
+      : leftArrow.classList.remove("disabled");
+    currentLevel == totalLevel
+      ? rightArrow.classList.add("disabled")
+      : rightArrow.classList.remove("disabled");
+    lvlActuel.innerHTML = currentLevel;
+    generateMap(currentLevel);
+  }
+});
+rightArrow.addEventListener("click", () => {
+  if (currentLevel < 6) {
+    currentLevel++;
+    lvlActuel.innerHTML = currentLevel;
+    currentLevel == 1
+      ? leftArrow.classList.add("disabled")
+      : leftArrow.classList.remove("disabled");
+    currentLevel == totalLevel
+      ? rightArrow.classList.add("disabled")
+      : rightArrow.classList.remove("disabled");
+    generateMap(currentLevel);
+  }
+});
+niveaux.forEach((element) => {
+  element.addEventListener("click", () => {
+    parseInt(element.innerHTML) == 1
+      ? leftArrow.classList.add("disabled")
+      : leftArrow.classList.remove("disabled");
+    parseInt(element.innerHTML) == 6
+      ? rightArrow.classList.add("disabled")
+      : rightArrow.classList.remove("disabled");
+    generateMap(parseInt(element.innerHTML));
+    lvlActuel.innerHTML = parseInt(element.innerHTML);
+  });
+});
 
+// ↓↓ CSS Text Logic
+buttonElement.addEventListener("click", () => {
+  let css = textAreaElement.value;
+  let player = document.querySelector("#Player");
+  if (!css.includes("transform:")) {
+    alert("Mauvaise Réponse");
+  } else {
+    player.style.cssText += css;
+  }
+});
 // Game Logic
 let playerCollidable = true;
 
-function collided() {
-    playerCollidable = false;
-    player.classList.toggle('escape');
-    setTimeout(() => {
-        alert("Good Job, You escaped level 1 !")
-    }, 2000);
-}
-
-function detectCollision() {
-    let playerRect = player.getBoundingClientRect();
-    let goalRect = goal.getBoundingClientRect();
-    
-    if (playerCollidable && 
-        playerRect.left < goalRect.left + goalRect.width &&
-        playerRect.left + playerRect.width > goalRect.left &&
-        playerRect.top < goalRect.top + goalRect.height &&
-        playerRect.height + playerRect.top > goalRect.top) {
-        collided();
+function collided(joueur) {
+  playerCollidable = false;
+  joueur.classList.toggle("escape");
+  setTimeout(() => {
+    if(currentLevel < totalLevel){
+      alert("Good Job, You escaped level "+currentLevel+" !");
+      currentLevel++;
+      generateMap(currentLevel);
     }
+    lvlActuel.innerHTML = currentLevel;
+  }, 2000);
 }
 
+function detectCollision(joueur, sortie) {
+  let playerRect = joueur.getBoundingClientRect();
+  let goalRect = sortie.getBoundingClientRect();
+
+  if (
+    playerCollidable &&
+    playerRect.left < goalRect.left + goalRect.width &&
+    playerRect.left + playerRect.width > goalRect.left &&
+    playerRect.top < goalRect.top + goalRect.height &&
+    playerRect.height + playerRect.top > goalRect.top
+  ) {
+    collided(joueur);
+  }
+}
 window.requestAnimationFrame(gameLoop);
 
 function gameLoop() {
-    detectCollision();
-    window.requestAnimationFrame(gameLoop);
+  let player = document.querySelector('#Player')
+  let goal = document.querySelector('#Exit')
+  detectCollision(player, goal);
+  window.requestAnimationFrame(gameLoop);
 }
--- maps.js
+++ maps.js
@@ -1,60 +1,139 @@
 // ↓↓ Génération de Map ↓↓
-function createTile(className) {
-    let div = document.createElement('div');
-    div.classList.add(className)
-    return div;
+function removeAllChildNodes(parent) {
+  while (parent.childElementCount > 1) {
+    parent.removeChild(parent.lastChild);
+  }
+}
+function createTile(className, id) {
+  let div = document.createElement("div");
+  if (className == null) {
+    div.setAttribute("id", id);
+  } else if (id == null) {
+    div.classList.add(className);
+  }
+  return div;
+}
+function initGame(levelIndex) {
+  let player = document.querySelector("#Player");
+  let goal = document.querySelector("#Exit");
+  let playerRect = player.getBoundingClientRect();
+  let goalRect = goal.getBoundingClientRect();
+
+  switch (levelIndex) {
+    case 1:
+      player.style.top = "4rem";
+      player.style.right = "unset";
+      player.style.bottom = "unset";
+      player.style.left = "unset";
+      break;
+
+    case 2:
+      player.style.top = "4rem";
+      player.style.right = "0";
+      player.style.bottom = "unset";
+      player.style.left = "unset";
+      break;
+
+    case 3:
+      player.style.top = "unset";
+      player.style.right = "unset";
+      player.style.bottom = "unset";
+      player.style.left = "4rem";
+      break;
+
+    default:
+      break;
+  }
 }
 
-// ↓↓ Première Map ↓↓
-if (labyrinth.dataset.level == "1"){
+function generateMap(levelIndex) {
+  switch (levelIndex) {
+    // ↓↓ Première Map ↓↓
+    case 1:
+      playerCollidable = true;
+      // ↓↓ Tiles Placement ↓↓
+      labyrinth.innerHTML = `
+      <div id="Playground-Wrap">
+          <div id="Playground">
+              <div id="Player"></div>
+          </div>
+      </div>`;
+      for (let i = 0; i < 36; i++) {
+        labyrinth.appendChild(createTile("wall"));
+      }
+      for (let i = 0; i < 9; i++) {
+        i == 8
+          ? labyrinth.appendChild(createTile(null, "Exit"))
+          : labyrinth.appendChild(createTile("path"));
+      }
+      for (let i = 0; i < 36; i++) {
+        labyrinth.appendChild(createTile("wall"));
+      }
 
-    // ↓↓ Player Placement ↓↓
-    player.style.top = "4rem";
+      initGame(levelIndex);
 
-    // ↓↓ Tiles Placement ↓↓
-    let tileCount = 0;
-    for (let i = 0; i < 36; i++) {    
-        tileCount++;  
-        labyrinth.appendChild(createTile('wall'));
-    }
-    for (let i = 0; i < 9; i++) {
-        tileCount++;  
-        if (i == 8){
-            labyrinth.appendChild(createTile('exit'));
-        }else{
-            labyrinth.appendChild(createTile('path'));
-        }
-    }
-    for (let i = 0; i < 36; i++) {   
-        tileCount++;     
-        labyrinth.appendChild(createTile('wall'));
-    }
+      // ↓↓ Gestion du texte ↓↓
+      break;
+    case 2:
+      playerCollidable = true;
+      // ↓↓ Tiles Placement ↓↓
+      labyrinth.innerHTML = `
+      <div id="Playground-Wrap">
+          <div id="Playground">
+              <div id="Player"></div>
+          </div>
+      </div>`;
+      for (let i = 0; i < 36; i++) {
+        labyrinth.appendChild(createTile("wall"));
+      }
+      for (let i = 0; i < 9; i++) {
+        i == 0
+          ? labyrinth.appendChild(createTile(null, "Exit"))
+          : labyrinth.appendChild(createTile("path"));
+      }
+      for (let i = 0; i < 36; i++) {
+        labyrinth.appendChild(createTile("wall"));
+      }
 
-    // ↓↓ Gestion du texte ↓↓
-}
+      initGame(levelIndex);
 
-// ↓↓ Seconde Map ↓↓
-if (labyrinth.dataset.level == "2"){
+      // ↓↓ Gestion du texte ↓↓
+      break;
 
-    // ↓↓ Player Placement ↓↓
-    player.style.left = "4rem";
-
-    // ↓↓ Tiles Placement ↓↓
-    let tileCount = 0;
-    for (let i = 0; i < 9; i++) {
+    // ↓↓ Troisième Map ↓↓
+    case 3:
+      playerCollidable = true;
+      // ↓↓ Tiles Placement ↓↓
+      labyrinth.innerHTML = `
+      <div id="Playground-Wrap">
+          <div id="Playground">
+              <div id="Player"></div>
+          </div>
+      </div>`;
+      for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            tileCount++;
-            if (tileCount == 77){
-                labyrinth.appendChild(createTile('exit'));
-            } else if (j == 4){
-                labyrinth.appendChild(createTile('path'));
-            } else {
-                labyrinth.appendChild(createTile('wall'));
-            }
+          if (j == 4) {
+            i == 8
+              ? labyrinth.appendChild(createTile(null, "Exit"))
+              : labyrinth.appendChild(createTile("path"));
+          } else {
+            labyrinth.appendChild(createTile("wall"));
+          }
         }
-    }
-    // ↓↓ Gestion du texte ↓↓
-
+      }
+      initGame(levelIndex);
+      // ↓↓ Gestion du texte ↓↓
+      break;
+    // ↓↓ Quatrième Map ↓↓
+    case 4:
+      break;
+    case 5:
+      break;
+    case 6:
+      break;
+    case 999:
+      break;
+    default:
+      break;
+  }
 }
-
-// ↓↓ Troisième Map ↓↓
